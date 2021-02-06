4982e804478b7b306f44c2cda452677d279c8e67
Andrew Hunter <andrewhunter1612@gmail.com>
2021-02-06 17:37:51 +0000

    vets page is starting to look tasty

--- vet_management_app/console.py
+++ vet_management_app/console.py
@@ -6,10 +6,14 @@ import repositories.vet_repository as vet_repository
 import repositories.owner_repository as owner_repository
 
 vet_1 = Vet("Vetty Voop")
+vet_2 = Vet("Davey Dave")
+vet_3 = Vet("Stevey Smith")
 owner_1 = Owner("Owen Owner", "0123456789", "221B Baker Street")
 animal_1 = Animal("Little horsey", "13/1/2020", "Horse", owner_1, "A good little horsey", vet_1)
 
 vet_repository.save_new_vet(vet_1)
+vet_repository.save_new_vet(vet_2)
+vet_repository.save_new_vet(vet_3)
 owner_repository.save_new_owner(owner_1)
 animal_repository.save_new_animal(animal_1)
 
